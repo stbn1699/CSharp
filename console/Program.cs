36338941a9b0162e0dc6fc03f37704ba1b017ecf
stbn1699 <stbnbasson@gmail.com>
2023-01-02 14:13:17 +0100

    first commit

---
+++ console/Program.cs
@@ -0,0 +1,23 @@
+﻿/*JoursSemaine aujourdhui = JoursSemaine.lundi;
+JoursSemaine demain = JoursSemaine.mardi;
+JoursSemaine weekend = JoursSemaine.weekend;
+
+Console.WriteLine("aujourd'hui, nous sommes un " + aujourdhui + " et demain un " + demain + " et le weekend c'est " + weekend);
+
+enum JoursSemaine
+{
+    lundi,
+    mardi,
+    mercredi,
+    jeudi,
+    vendredi,
+    samedi,
+    dimanche,
+    weekend = samedi | dimanche,
+}*/
+
+/*int[][] tableauTwo = new int[3][];
+tableauTwo[0] = new int[] {45, 2};
+tableauTwo[1] = new int[] {34, 34, 4, 67};
+
+Console.WriteLine(tableauTwo[0][1]);*/
---
+++ tp1/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Écrivez un programme qui demande à l'utilisateur deux nombres, puis affiche le plus grand des deux nombres. Utilisez une instruction if-else pour déterminer le plus grand nombre. */
+
+Console.WriteLine("");
+Console.WriteLine("veuillez entrer le premier nombre : ");
+string firstNumber = Console.ReadLine();
+Console.WriteLine("");
+Console.WriteLine("veuillez entrer le deuxieme nombre : ");
+string secondNumber = Console.ReadLine();
+int.Parse(secondNumber);
